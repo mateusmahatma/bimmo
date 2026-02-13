616e80218ed4624b0124c7cfa0b7607608d8dd1c
mateusmahatma <mateusmahatma@gmail.com>
2026-02-13 10:02:55 +0700

    Update tombol.blade.php

--- resources/views/pengeluaran/tombol.blade.php
+++ resources/views/pengeluaran/tombol.blade.php
@@ -1,13 +1,30 @@
-<div class="dropdown-sidebar">
-    <button class="icon-elipsis" data-bs-toggle="dropdown" aria-expanded="false">
-        &#8943;
+<div class="dropdown text-center">
+    <button class="btn btn-light btn-sm"
+        type="button"
+        data-bs-toggle="dropdown"
+        data-bs-boundary="viewport"
+        data-bs-display="static"
+        aria-expanded="false">
+        <i class="bi bi-three-dots-vertical"></i>
     </button>
-    <ul class="dropdown-menu">
+
+    <ul class="dropdown-menu dropdown-menu-end shadow-sm">
         <li>
-            <a class="dropdown-item" href="{{ route('pengeluaran.edit', $request->id) }}">
-                Edit
+            <a class="dropdown-item tombol-edit-pengeluaran"
+                href="#"
+                data-id="{{ $request->id }}">
+                <i class="bi bi-pencil me-2 text-warning"></i> Edit
             </a>
         </li>
-        <li><a class="dropdown-item tombol-del-pengeluaran" href="#" data-id="{{ $request->id }}">Delete</a></li>
+
+        <li><hr class="dropdown-divider"></li>
+
+        <li>
+            <a class="dropdown-item tombol-del-pengeluaran text-danger"
+                href="#"
+                data-id="{{ $request->id }}">
+                <i class="bi bi-trash me-2"></i> Delete
+            </a>
+        </li>
     </ul>
 </div>
