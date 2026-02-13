c4c935e5f71364a4eaa4accb3bbb6569f1ab8871
mateusmahatma <mateusmahatma@gmail.com>
2026-02-13 10:02:27 +0700

    Update tombol.blade.php

--- resources/views/kalkulator/tombol.blade.php
+++ resources/views/kalkulator/tombol.blade.php
@@ -1,14 +1,24 @@
-<div class="dropdown-sidebar">
-    <button class="icon-elipsis" data-bs-toggle="dropdown" aria-expanded="false">
-        &#8943;
+<div class="dropdown">
+    <button class="btn btn-light btn-sm text-secondary" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <i class="bi bi-three-dots-vertical"></i>
     </button>
-    <ul class="dropdown-menu">
+    <ul class="dropdown-menu dropdown-menu-end border-0 shadow-sm">
         <li>
             <a class="dropdown-item" href="{{ route('kalkulator.show', $request->id_proses_anggaran) }}">
-                Detail
+                <i class="bi bi-eye me-2 text-primary"></i> Detail
             </a>
         </li>
-        <li><a class="dropdown-item tombol-update-proses-anggaran" href="#" data-id="{{ $request->id_proses_anggaran }}">Update</a></li>
-        <li><a class="dropdown-item tombol-del-proses-anggaran" href="#" data-id="{{ $request->id_proses_anggaran }}">Delete</a></li>
+        <li>
+             <!-- Update Button -->
+            <a class="dropdown-item tombol-update-proses-anggaran" href="#" data-id="{{ $request->id_proses_anggaran }}">
+                <i class="bi bi-arrow-repeat me-2 text-warning"></i> Update
+            </a>
+        </li>
+        <li><hr class="dropdown-divider"></li>
+        <li>
+            <a class="dropdown-item tombol-del-proses-anggaran text-danger" href="#" data-id="{{ $request->id_proses_anggaran }}">
+                <i class="bi bi-trash me-2"></i> Delete
+            </a>
+        </li>
     </ul>
 </div>
