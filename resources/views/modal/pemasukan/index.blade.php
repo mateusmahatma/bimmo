1b163abdbac9abfca35d321f93893f462faa22b2
mateusmahatma <mateusmahatma@gmail.com>
2026-02-13 10:02:40 +0700

    Update index.blade.php

--- resources/views/modal/pemasukan/index.blade.php
+++ resources/views/modal/pemasukan/index.blade.php
@@ -1,21 +1,19 @@
 <div class="modal fade" id="pemasukanModal" tabindex="-1" aria-labelledby="pemasukanModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h1 class="modal-title fs-5" id="pemasukanModalLabel">Input Jenis Pemasukan</h1>
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content border-0 shadow">
+            <div class="modal-header border-bottom-0 pb-0">
+                <h5 class="modal-title fw-bold" id="pemasukanModalLabel">Income Category</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body">
-                <div class="mb-3 row">
-                    <label for="nama" class="col-sm-2 col-form-label">Nama</label>
-                    <div class="col-sm-10">
-                        <input type="text" id="nama" class="form-control" name='nama' placeholder="Input Jenis Pemasukan" required>
-                    </div>
+            <div class="modal-body pt-4">
+                <div class="mb-3">
+                    <label for="nama" class="form-label fw-medium small text-uppercase text-muted">Category Name</label>
+                    <input type="text" id="nama" class="form-control" name="nama" placeholder="e.g. Salary, Bonus, Freelance" required>
                 </div>
             </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Tutup</button>
-                <button type="button" class="btn btn-success tombol-simpan-pemasukan">Simpan</button>
+            <div class="modal-footer border-top-0 pt-0 pb-4">
+                <button type="button" class="btn btn-light" data-bs-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-primary px-4 tombol-simpan-pemasukan">Save</button>
             </div>
         </div>
     </div>
