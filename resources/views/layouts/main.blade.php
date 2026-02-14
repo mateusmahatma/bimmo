556217468ea858e1b1a4cae28c3c7f2410ea5949
mateusmahatma <mateusmahatma@gmail.com>
2026-02-14 22:37:08 +0700

    Update main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,11 +3,31 @@
 @section('body')
 <div class="container-fluid py-3">
     <div class="row">
-        <aside class="col-md-2 bg-light border-end">
-            @include('layouts.sidebar')
+        {{-- DESKTOP SIDEBAR --}}
+        <aside class="col-md-2 bg-light border-end d-none d-md-block">
+            @include('layouts.sidebar', ['prefix' => 'desktop_'])
         </aside>
 
-        <main class="col-md-10">
+        {{-- MOBILE OFFCANVAS SIDEBAR --}}
+        <div class="offcanvas offcanvas-start" tabindex="-1" id="mobileSidebar" aria-labelledby="mobileSidebarLabel">
+            <div class="offcanvas-header border-bottom">
+                <h5 class="offcanvas-title" id="mobileSidebarLabel">Menu</h5>
+                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body p-0">
+                @include('layouts.sidebar', ['prefix' => 'mobile_'])
+            </div>
+        </div>
+
+        <main class="col-md-10 ms-sm-auto px-md-4">
+            {{-- MOBILE TOGGLE BUTTON --}}
+            <div class="d-md-none py-2 mb-3 border-bottom d-flex align-items-center">
+                <button class="btn btn-outline-secondary btn-sm me-2" type="button" data-bs-toggle="offcanvas" data-bs-target="#mobileSidebar" aria-controls="mobileSidebar">
+                    <i class="bi bi-list fs-5"></i>
+                </button>
+                <img src="{{ asset('img/bimmo_2.png') }}" alt="BIMMO" style="height: 25px;">
+            </div>
+
             @yield('container')
         </main>
     </div>
