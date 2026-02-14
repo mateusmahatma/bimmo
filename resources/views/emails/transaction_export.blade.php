6461b2a6339f5c4be0edf8240ccdf58621d861b0
mateusmahatma <mateusmahatma@gmail.com>
2026-02-14 22:00:00 +0700

    Create transaction_export.blade.php

---
+++ resources/views/emails/transaction_export.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Export Data Transaksi</title>
+</head>
+<body>
+    <h2>Halo, Pengguna BIMMO!</h2>
+    <p>Berikut adalah file export data transaksi Anda yang dilampirkan dalam format Excel.</p>
+    <p>Terima kasih telah menggunakan BIMMO untuk mengelola keuangan Anda.</p>
+    <br>
+    <p>Salam,</p>
+    <p>Tim BIMMO</p>
+</body>
+</html>
