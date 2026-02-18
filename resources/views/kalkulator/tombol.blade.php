64862bfe2a557df40e55e988de6f9ef6b31f460c
mateusmahatma <mateusmahatma@gmail.com>
2026-02-18 12:20:34 +0700

    Update tombol.blade.php

--- resources/views/kalkulator/tombol.blade.php
+++ resources/views/kalkulator/tombol.blade.php
@@ -4,19 +4,19 @@
     </button>
     <ul class="dropdown-menu dropdown-menu-end border-0 shadow-sm">
         <li>
-            <a class="dropdown-item" href="{{ route('kalkulator.show', $request->id_proses_anggaran) }}">
+            <a class="dropdown-item" href="{{ route('kalkulator.show', $request->hash) }}">
                 <i class="bi bi-eye me-2 text-primary"></i> Detail
             </a>
         </li>
         <li>
              <!-- Update Button -->
-            <a class="dropdown-item tombol-update-proses-anggaran" href="#" data-id="{{ $request->id_proses_anggaran }}">
+            <a class="dropdown-item tombol-update-proses-anggaran" href="#" data-id="{{ $request->hash }}">
                 <i class="bi bi-arrow-repeat me-2 text-warning"></i> Update
             </a>
         </li>
         <li><hr class="dropdown-divider"></li>
         <li>
-            <a class="dropdown-item tombol-del-proses-anggaran text-danger" href="#" data-id="{{ $request->id_proses_anggaran }}">
+            <a class="dropdown-item tombol-del-proses-anggaran text-danger" href="#" data-id="{{ $request->hash }}">
                 <i class="bi bi-trash me-2"></i> Delete
             </a>
         </li>
