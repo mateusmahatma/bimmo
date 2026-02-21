6cdd04f00924050258a3357df397aba63c34193a
mateusmahatma <mateusmahatma@gmail.com>
2026-02-22 06:10:20 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>@yield('title','BIMMO')</title>
+    <title>@yield('title','Bimmo')</title>
 
     <link rel="icon" href="{{ asset('img/bimmo_favicon.png') }}" type="image/x-icon">
 
