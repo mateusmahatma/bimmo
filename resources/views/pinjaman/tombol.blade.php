ac5cf10349c791ffc2e3c368fa78998960f52234
mateusmahatma <mateusmahatma@gmail.com>
2026-02-21 16:49:26 +0700

    Update tombol.blade.php

--- resources/views/pinjaman/tombol.blade.php
+++ resources/views/pinjaman/tombol.blade.php
@@ -3,8 +3,8 @@
         &#8943;
     </button>
     <ul class="dropdown-menu">
-        <li><a class="dropdown-item tombol-edit-detail" href="{{ route('pinjaman.show', $pinjaman->id) }}" data-id="#">Detail</a></li>
-        <li><a class="dropdown-item tombol-edit-pinjaman" href="#" data-id="{{ $request->id }}">Edit</a></li>
-        <li><a class="dropdown-item tombol-del-pinjaman" href="#" data-id="{{ $pinjaman->id }}">Delete</a></li>
+        <li><a class="dropdown-item tombol-edit-detail" href="{{ route('pinjaman.show', $pinjaman->hash) }}" data-id="#">Detail</a></li>
+        <li><a class="dropdown-item tombol-edit-pinjaman" href="#" data-id="{{ $pinjaman->hash }}">Edit</a></li>
+        <li><a class="dropdown-item tombol-del-pinjaman" href="#" data-id="{{ $pinjaman->hash }}">Delete</a></li>
     </ul>
 </div>
