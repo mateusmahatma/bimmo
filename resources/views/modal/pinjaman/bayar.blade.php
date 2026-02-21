4db74eda7bc46a21cd5d785ee74131fbc6690989
mateusmahatma <mateusmahatma@gmail.com>
2026-02-21 20:35:05 +0700

    Update bayar.blade.php

--- resources/views/modal/pinjaman/bayar.blade.php
+++ resources/views/modal/pinjaman/bayar.blade.php
@@ -18,6 +18,14 @@
                         <label for="tgl_bayar" class="col-form-label">Tanggal Pembayaran</label>
                         <input type="date" id="tgl_bayar" name="tgl_bayar" class="form-control" required>
                     </div>
+                    <div class="mb-3">
+                        <label for="bukti_bayar" class="col-form-label">Bukti Bayar (Opsional)</label>
+                        <input type="file" id="bukti_bayar" name="bukti_bayar" class="form-control" accept=".jpg,.jpeg,.png,.pdf">
+                        <div id="current_file_container" class="mt-2 d-none">
+                            <span class="small text-muted">File saat ini: </span>
+                            <a id="current_file_link" href="#" target="_blank" class="small">Lihat Berkas</a>
+                        </div>
+                    </div>
                     <div class="modal-footer">
                         <button type="button" class="btn-sm btn-color2" data-bs-dismiss="modal">
                             <i class="fa fa-times"></i> Tutup
