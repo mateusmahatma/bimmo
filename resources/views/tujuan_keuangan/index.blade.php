c444b5509d7ccbc7f5f7f61757b4e31bb8f93bea
mateusmahatma <mateusmahatma@gmail.com>
2026-03-07 12:29:13 +0700

    Fix PWA Display of Financial Goals Menu

--- resources/views/tujuan_keuangan/index.blade.php
+++ resources/views/tujuan_keuangan/index.blade.php
@@ -4,6 +4,7 @@
 
 @push('css')
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.4.1/css/responsive.bootstrap5.min.css">
 <style>
     .goal-card {
         transition: transform 0.2s;
@@ -246,6 +247,8 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.4.1/js/dataTables.responsive.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.4.1/js/responsive.bootstrap5.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="{{ asset('js/tujuan-keuangan.js') }}"></script>
 @endpush
