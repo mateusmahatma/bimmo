ab003adead3ef91e064beef648c5dcf31df1af33
mateusmahatma <mateusmahatma@gmail.com>
2026-03-08 18:33:50 +0700

    Create PemasukanTemplateExport.php

---
+++ app/Exports/PemasukanTemplateExport.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Exports;
+
+use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\WithMultipleSheets;
+
+class PemasukanTemplateExport implements WithMultipleSheets
+{
+    use Exportable;
+
+    public function sheets(): array
+    {
+        return [
+            new Sheets\PemasukanCategoryImportSheet(),
+        ];
+    }
+}
