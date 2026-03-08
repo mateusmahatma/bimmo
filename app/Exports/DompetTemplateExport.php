14f2a496091eaea5f498c1d4c1f1f222271e26d7
mateusmahatma <mateusmahatma@gmail.com>
2026-03-08 18:33:45 +0700

    Create DompetTemplateExport.php

---
+++ app/Exports/DompetTemplateExport.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Exports;
+
+use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\WithMultipleSheets;
+
+class DompetTemplateExport implements WithMultipleSheets
+{
+    use Exportable;
+
+    public function sheets(): array
+    {
+        return [
+            new Sheets\DompetImportSheet(),
+        ];
+    }
+}
