ad6521460ec9cfa4a2bb2dc9a0d05c2fc9d6df02
mateusmahatma <mateusmahatma@gmail.com>
2026-03-09 22:41:34 +0700

    Fixed the sidebar color for PWA dark mode.

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,7 +10,7 @@
         </aside>
 
         {{-- MOBILE OFFCANVAS SIDEBAR --}}
-        <div class="offcanvas offcanvas-start" tabindex="-1" id="mobileSidebar" aria-labelledby="mobileSidebarLabel">
+        <div class="offcanvas offcanvas-start" tabindex="-1" id="mobileSidebar" aria-labelledby="mobileSidebarLabel" data-bs-theme="light">
             <div class="offcanvas-header border-bottom">
                 <h5 class="offcanvas-title" id="mobileSidebarLabel">Menu</h5>
                 <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
